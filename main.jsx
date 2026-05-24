2645552227017a3c1bf4228950a729401877f0f8
Aaron Marchant <aaronmarchant96@gmail.com>
2026-05-24 02:00:50 -0600

    Add root React entrypoint

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DebateFurnace from './src/DebateFurnace.jsx';
+import './src/style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DebateFurnace />
+  </React.StrictMode>
+);
