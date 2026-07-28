b23b5ff7f067c4ad831a95ba93b57cc6de0e5707
PromptHound <prompthound.ai@gmail.com>
2026-07-27 21:44:09 -0600

    feat: add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import AppShell from "./AppShell.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./tailwind.css";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppShell />
+    <SpeedInsights />
   </React.StrictMode>
 );
